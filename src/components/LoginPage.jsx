83d3a7bfc429e161748129f1e4a67bc6c129c746
imas8888 <zcahsuk@ucl.ac.uk>
2022-11-02 16:18:38 +0000

    Fixed login page

--- src/components/LoginPage.jsx
+++ src/components/LoginPage.jsx
@@ -1,22 +1,31 @@
-import React from "react";
-import backgroundImage from "../assets/Screen 1 BG.png"
-
-const onLoginFormSubmit = () => {
-    return 0;
-}
+import React, { useState } from "react";
+import backgroundImage from "../assets/Screen 1 BG.png";
 
 export const LoginPage = () => {
+    const [username, setUsername] = useState('');
+    const [password, setPassword] = useState('');
+
+    const onLoginFormSubmit = (e) => {
+        if (username.toLowerCase() == 'sonia.nygaard' && password == 'HeISeNBeRg') {
+            // Route to email page
+        } else {
+            e.preventDefault();
+            document.querySelector('.loginError').style.display = 'block';
+        }
+    }
+
     return (
         <div>
             <h1 className="loginTitle">SLOUGH PC 1</h1>
             <img className="loginBackground" src={backgroundImage} alt="backgroundImage" />
             <form className="loginForm" onSubmit={onLoginFormSubmit}>
                 <label for="uname">Username:</label><br />
-                <input className="loginInput" type="text" id="uname" name="uname" /><br />
+                <input className="loginInput" type="text" onChange={event => setUsername(event.target.value)} /><br />
                 <label for="passwd">Password:</label><br />
-                <input className="loginInput" type="password" id="passwd" name="passwd" />
+                <input className="loginInput" type="password" onChange={event => setPassword(event.target.value)} />
                 <button type="submit" style={{ display: "none" }} />
             </form>
+            <h2 className="loginError">Incorrect Username or Password</h2>
         </div>
     )
 }
